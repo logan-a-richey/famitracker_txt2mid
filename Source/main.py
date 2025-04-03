d3af98308d3de31e3151f6c32e090f3b4936ceda
logan-a-richey <loganricheygithub@gmail.com>
2025-04-03 11:39:23 -0500

    Initial commit.

---
+++ Source/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+# stop creation of __pycache__
+sys.dont_write_bytecode = True
+
+
+
+def main():
+    print("Hello Famitracker world!")
+
+if __name__ == "__main__":
+    main()
+
