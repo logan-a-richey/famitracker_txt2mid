233e6c33d76cb4421d478604d110ea9f08da7aec
logan-a-richey <loganricheygithub@gmail.com>
2025-04-07 15:43:05 -0500

    Refactor create_validation_json.py with sep of concerns.

--- Scripts/create_validation_json.py
+++ Scripts/create_validation_json.py
@@ -7,17 +7,116 @@ import re
 import json
 import logging
 
-sys.dont_write_bytecode = True # stop creation of __pycache__
+# stop creation of __pycache__
+sys.dont_write_bytecode = True 
 
+# init logging path if it does not exist
 LOG_DIR = '/tmp/logs'
-LOG_FILE_PATH = LOG_DIR + '/create_json_check.log'
 os.makedirs(LOG_DIR, exist_ok=True)
 
-class CreateJson:
-    def __init__(self, log_to_console=True):
+# create the logging path
+LOG_FILE_PATH = LOG_DIR + '/create_json_check.log'
+
+# files to read and write
+ifile = os.path.abspath("../Docs/famitracker_export_data.txt")
+ofile = os.path.abspath("../Docs/famitracker_export_data.json")
+
+
+class CreateJsonReader:
+    ''' Helper class to parse famitracker_export_data.txt into a dictionary for json creation. '''
+    
+    def __init__(self, parent) -> None:
+        self.parent = parent
         self.data = {}
         self.last_tag = ""
 
+    def _handle_tag(self, line) -> None:
+        tag = line.split()[0].upper()
+        self.data[tag] = {}
+        self.data[tag]["FIELDS"] = {}
+        self.last_tag = tag
+    
+    def _handle_data(self, line) -> None:
+        field_name = ""
+        field_data_type = ""
+        field_range_lower = ""
+        field_range_upper = ""
+        
+        try:
+            word_match = re.findall(r'\w+', line)
+            if word_match:
+                field_name = word_match[0]
+                field_data_type = word_match[1]
+                
+                if word_match[1] == "string":
+                    pass
+                else:
+                    range_match = re.findall(r'\-?[0-9A-F]+', line)
+                    if range_match: 
+                        field_range_lower = range_match[0]
+                        field_range_upper = range_match[1]
+                
+                # Display data to be used in json in an ascii text table. Writes to logger and stdout.
+                self.parent.writer.print_debug_table_line(
+                    field_name, field_data_type, field_range_lower, field_range_upper
+                )
+
+        except Exception as e:          
+            self.logger.error("ERROR {} \nLINE \'{}\'".format(e, line))
+            exit(1)
+        
+        self.data[self.last_tag]["FIELDS"][field_name] = {}
+        this_field_name = self.data[self.last_tag]["FIELDS"][field_name] 
+        this_field_name["TYPE"] = field_data_type
+        this_field_name["RANGE"] = {}
+        this_field_name["RANGE"]["LOWER"] = field_range_lower
+        this_field_name["RANGE"]["UPPER"] = field_range_upper
+
+    def _process_line(self, line) -> None:
+        if line[0].isupper():
+            self._handle_tag(line)
+        elif line[0] == "-":
+            self._handle_data(line)
+
+    def read_file(self, ifile):
+        with open(ifile, "r") as file:
+            for line in file:
+                line = line.strip()
+                if not line:
+                    continue
+                if line[0] == "#":
+                    continue
+                self._process_line(line)
+
+
+class CreateJsonWriter:
+    ''' Helper class to writes logging info & create the json file. '''
+
+    def __init__(self, parent):
+        self.parent = parent
+
+    def print_debug_table_line(self, field_name, field_data_type, field_range_lower, field_range_upper):
+        params = [field_name, field_data_type, field_range_lower, field_range_upper]
+        self.parent.logger.info("TAG: {} DATA_TYPE: {} RANGE_LOWER: {} RANGE_UPPER: {}".format( \
+            *["\'{}\'".format(item).ljust(20) for item in params])
+        )
+
+    def write_file(self, ofile):
+        with open(ofile, "w") as file:
+            file.write(json.dumps(self.parent.reader.data, indent=2))
+        self.parent.logger.info("File \'{}\' has been created.".format(ofile))
+
+
+class CreateJson:
+    '''
+    Reads famitracker_export_data.txt. Converts it into .json format for later use.
+    Contains a CreateJsonReader and a CreateJsonWriter object.
+    Contains logic for handling the logger. The logger can write to both file and stdout.
+    '''
+    def __init__(self, log_to_console=True):
+        self.reader = CreateJsonReader(self)
+        self.writer = CreateJsonWriter(self)
+        
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
 
@@ -40,86 +139,11 @@ class CreateJson:
                 console_handler.setFormatter(formatter)
                 self.logger.addHandler(console_handler)
         
-    def print_debug_table_line(self, field_name, field_data_type, field_range_lower, field_range_upper):
-        self.logger.info("TAG: {} DATA_TYPE: {} RANGE_LOWER: {} RANGE_UPPER: {}".format( \
-            *["\'{}\'".format(item).ljust(20) for item in [field_name, field_data_type, field_range_lower, field_range_upper]])
-        )    
-
-    def _process_line(self, line):
-        # tag line
-        if line[0].isupper():
-            tag = line.split()[0].upper()
-            self.data[tag] = {}
-            self.data[tag]["FIELDS"] = {}
-            self.last_tag = tag
-            return
-        
-        # field data
-        elif line[0] == "-":
-            if line.startswith("- Defines"):
-                return
-
-            field_name = ""
-            field_data_type = ""
-            field_range_lower = ""
-            field_range_upper = ""
-            
-            try:
-                word_match = re.findall(r'\w+', line)
-                if word_match:
-                    field_name = word_match[0]
-                    field_data_type = word_match[1]
-                    
-                    if word_match[1] == "string":
-                        pass
-                    else:
-                        range_match = re.findall(r'\-?[0-9A-F]+', line)
-                        if range_match: 
-                            field_range_lower = range_match[0]
-                            field_range_upper = range_match[1]
-                    
-                    # printed to log
-                    self.print_debug_table_line(field_name, field_data_type, field_range_lower, field_range_upper)
-
-            except Exception as e:          
-                self.logger.error("ERROR {} \nLINE \'{}\'".format(e, line))
-                exit(1)
-            
-            field_range = (field_range_lower, field_range_upper)
-            
-            self.data[self.last_tag]["FIELDS"][field_name] = {}
-            this_field_name = self.data[self.last_tag]["FIELDS"][field_name] 
-            this_field_name["TYPE"] = field_data_type
-            this_field_name["RANGE"] = {}
-            this_field_name["RANGE"]["LOWER"] = field_range_lower
-            this_field_name["RANGE"]["UPPER"] = field_range_upper
-            
-    def read_file(self, ifile):
-        with open(ifile, "r") as file:
-            for line in file:
-                line = line.strip()
-                if not line:
-                    continue
-                if line[0] == "#":
-                    continue
-                self._process_line(line)
-    
-    def write_file(self, ofile):
-        with open(ofile, "w") as file:
-            file.write(json.dumps(self.data, indent=2))
-            #file.write(json.dumps(self.data))
-            
-        self.logger.info("File \'{}\' has been created.".format(ofile))
-
 
 def main():
-    #ifile = get_input_file()
-    ifile = os.path.abspath("../Docs/famitracker_export_data.txt")
-    ofile = os.path.abspath("../Docs/famitracker_export_data.json")
-
     cj = CreateJson()
-    cj.read_file(ifile)
-    cj.write_file(ofile) 
+    cj.reader.read_file(ifile)
+    cj.writer.write_file(ofile) 
 
 if __name__ == "__main__":
     main()
