ced241ebc8a31bd648b67117423ec1ea9417ff1c
logan-a-richey <loganricheygithub@gmail.com>
2025-04-09 16:15:44 -0500

    WIP - progress on regex matchers

--- RegexVersion/stages/reader.py
+++ RegexVersion/stages/reader.py
@@ -6,41 +6,73 @@ from containers.macro import Macro
 #from containers.instrument import Instrument
 #from containers.track import Track
 
+# TODO handle regex not matching error
+
 class Reader:
     def __init__(self, project):
         self.project = project
-            
+        
+        self.matchers = [
+            (
+                re.compile(r'^\s*(TITLE|AUTHOR|COPYRIGHT|COMMENT)\s+\"(.*?)"$'),
+                self.handle_song_information
+            ),
+            (
+                re.compile(r'^\s*(MACHINE|FRAMERATE|EXPANSION|VIBRATO|SPLIT|N163CHANNELS)\s+(\d+)\s*$'),
+                self.handle_global_settings
+            ),
+            (
+                re.compile(r'^\s*(MACRO\w*)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s+(-?\d+)\s*:\s*((?:-?\d+\s*)+)$'),
+                self.handle_macros
+            )
+            # TODO INSTRUMENT
+            # TODO GROOVE
+            # TODO DPCM
+            # TODO DPCMKEY
+            # TODO SPECIAL MAcros and instrument settings
+            # TODO TRACK
+            # TODO COLUMN
+            # TODO ORDER 
+            # TODO PATTERN 
+            # TODO ROW
+        ]
+    
+    def handle_song_information(self, regex_match_object):
+        self.project.song_information[regex_match_object.group(1)] = regex_match_object.group(2)
+
+    def handle_global_settings(self, regex_match_object):
+        self.project.global_settings[regex_match_object.group(1)] = int(regex_match_object.group(2))
+
+    def handle_macros(self, regex_match_object):
+        # re.match(r'''
+        # ^\s*               # Optional starting whitespace
+        # (MACRO\w*)         # Group 1: MACRO tag (e.g., MACRO, MACRO_A)
+        # \s+
+        # (-?\d+)\s+         # Group 2: type
+        # (-?\d+)\s+         # Group 3: index
+        # (-?\d+)\s+         # Group 4: loop
+        # (-?\d+)\s+         # Group 5: release
+        # (-?\d+)\s*         # Group 6: setting
+        # :\s*
+        # ((?:-?\d+\s*)+)    # Group 7: macro sequence
+        # $
+        
+        macro_tag = regex_match_object.group(1)
+        macro_type, macro_index, macro_loop, macro_release, macro_setting = list(map(int, regex_match_object.group(2, 3, 4, 5, 6)))
+        macro_sequence = list(map(int, re.findall(r'-?\d+', regex_match_object.group(7))))
+        macro_key = "{}.{}.{}".format(macro_tag, macro_type, macro_index)
+        macro_object = Macro(macro_tag, macro_type, macro_index, macro_loop, macro_release, macro_setting, macro_sequence)
+        self.project.macros[macro_key] = macro_object
+        print(macro_object)
+        # TODO debug exit
+        exit(0)
+
     def process_line(self, line: str) -> None:
-        if x := re.match(r'^\s*(TITLE|AUTHOR|COPYRIGHT|COMMENT)\s+\"(.*?)"$', line):
-            self.project.song_information[x.group(1)] = x.group(2)
-        
-        elif x := re.match(r'^\s*(MACHINE|FRAMERATE|EXPANSION|VIBRATO|SPLIT|N163CHANNELS)\s+(\d+)\s*$', line):
-            self.project.global_settings[x.group(1)] = int(x.group(2))
-            #print(x.group(1), x.group(2))
-        
-        elif x := re.match(r'''
-            ^\s*
-            (MACRO\w*)         # Group 1: MACRO tag (e.g., MACRO, MACRO_A)
-            \s+
-            (-?\d+)\s+         # Group 2: type
-            (-?\d+)\s+         # Group 3: index
-            (-?\d+)\s+         # Group 4: loop
-            (-?\d+)\s+         # Group 5: release
-            (-?\d+)\s*         # Group 6: setting
-            :\s*
-            ((?:-?\d+\s*)+)    # Group 7: macro sequence
-            $
-        ''', line, re.VERBOSE):
-            macro_tag = x.group(1)
-            macro_type, macro_index, macro_loop, macro_release, macro_setting = list(map(int, x.group(2, 3, 4, 5, 6)))
-            macro_sequence = list(map(int, re.findall(r'-?\d+', x.group(7))))
-            macro_key = "{}.{}.{}".format(macro_tag, macro_type, macro_index)
-            macro_object = Macro(macro_tag, macro_type, macro_index, macro_loop, macro_release, macro_setting, macro_sequence)
-            self.project.macros[macro_key] = macro_object
-            print(macro_object)
-            exit()
-        else:
-            print("Unknown line: {}".format(line))
+        for regex_pattern, function in self.matchers:
+            if x := regex_pattern.match(line):
+                function(x)
+            else:
+                print("Unknown line: {}".format(line))
         return
     
     def read(self, input_file) -> None:
